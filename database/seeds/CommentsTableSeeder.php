5dc615b2b3665db682d91fbc04820b1dbeff995a
SHjNEe <baotrung.jpvn@gmail.com>
2023-03-19 22:09:36 +0700

    Update

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -21,9 +21,16 @@ class CommentsTableSeeder extends Seeder
         $commentsCount = (int)$this->command->ask('How many comments would you like?', 150);
 
         $users = App\User::all();
-        
+
         factory(App\Comment::class, $commentsCount)->make()->each(function ($comment) use ($posts, $users) {
-            $comment->blog_post_id = $posts->random()->id;
+            $comment->commentable_id = $posts->random()->id;
+            $comment->commentable_type = 'App\BlogPost';
+            $comment->user_id = $users->random()->id;
+            $comment->save();
+        });
+        factory(App\Comment::class, $commentsCount)->make()->each(function ($comment) use ($users) {
+            $comment->commentable_id = $users->random()->id;
+            $comment->commentable_type = 'App\User';
             $comment->user_id = $users->random()->id;
             $comment->save();
         });
